fc69bfbe8b7bb18d6509d1214d20a5d3d3d169c3
Steven <Zsteven44@gmail.com>
2016-09-21 20:40:27 -0400

    First commit

---
+++ Assets/_Scripts/sortAlgorithm.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class sortAlgorithm : MonoBehaviour {
+	private GameObject[] s;
+	private ArrayList myNodes;
+	private int items;
+	private int index;
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i < 5; i++) {
+			
+			s[i] = new GameObject();
+			s[i] = GameObject.CreatePrimitive (PrimitiveType.Sphere);
+			s[i].transform.position = new Vector3(i*2, i*2, 0);
+
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
